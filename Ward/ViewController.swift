de30685182480c0a1547e7ee71ad2848ad3e0385
keem-hyun <hw813@naver.com>
2024-01-02 11:56:58 +0900

    init

---
+++ Ward/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ward
+//
+//  Created by 김강현 on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
