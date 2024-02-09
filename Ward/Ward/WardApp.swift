dbae029148066cfc000a38b06f8045af8b72ac90
EunJI <83203198+ejalice@users.noreply.github.com>
2024-02-10 00:00:57 +0900

    [ADD] Create Project

---
+++ Ward/Ward/WardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardApp.swift
+//  Ward
+//
+//  Created by eunji on 2024/02/09.
+//
+
+import SwiftUI
+
+@main
+struct WardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
