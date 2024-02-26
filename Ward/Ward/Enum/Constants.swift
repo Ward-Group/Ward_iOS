c0344db6e5107b9e1efb9971ba3d6f16d907b904
강신규 <rkdtlscks123@naver.com>
2024-02-26 17:52:06 +0900

    [ADD] URLString Constants Enum Setting
    URLString Constants Enum Setting

---
+++ Ward/Ward/Enum/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Ward
+//
+//  Created by 강신규 on 2/26/24.
+//
+
+import Foundation
+
+enum Constants {
+    
+    // Login 상태 확인
+    static let LOGIN_STATE = false
+    
+    
+
+}
---
+++ Ward/Ward/Enum/URL.swift
@@ -0,0 +1,21 @@
+//
+//  URL.swift
+//  Ward
+//
+//  Created by 강신규 on 2/26/24.
+//
+
+import Foundation
+
+
+enum URLString {
+    
+    //TODO 실제 URL 세팅
+    static let mode = "DEBUG"
+    static let baseURL = "https://app.ward.co.kr"
+    
+//    static let mode = "PRODUCTION"
+//    static let baseURL = "https://app.ward.co.kr"
+    
+
+}
