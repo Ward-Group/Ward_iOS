cef1352e6d8702ed5f89b60ed9ae1ce2ed1736d3
peppermint100 <krpeppermint100@gmail.com>
2024-05-01 18:09:34 +0900

    chore : 이전 프로젝트 파일 삭제, 새로운 SwiftUI 프로젝트 추가

---
+++ WardApp/WardApp/WardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardAppApp.swift
+//  WardApp
+//
+//  Created by peppermint100 on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
