5559842c7de6b3e4b4f7936ca1aa1a0c94df0b3f
peppermint100 <krpeppermint100@gmail.com>
2024-05-27 12:50:11 +0900

    feat: Apple Login 버튼 추가, View 내에서 Apple 로그인 요청

--- WardApp/WardApp/Presentation/Login/LoginView.swift
+++ WardApp/WardApp/Presentation/Login/LoginView.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+import Combine
 import AuthenticationServices
 
 struct LoginView: View {
+    
+    @ObservedObject var input: LoginViewModel.Input
+    @ObservedObject var output: LoginViewModel.Output
+    let cancelBag = CancelBag()
+    
+    let userTrigger = PassthroughSubject<UserFromLoginProvider, Never>()
+    
+    init(vm: LoginViewModel) {
+        let input = LoginViewModel.Input(
+            userTrigger: userTrigger
+        )
+        output = vm.transform(input, cancelBag: cancelBag)
+        self.input = input
+    }
     
     var body: some View {
         VStack {
@@ -18,6 +33,7 @@ struct LoginView: View {
 }
 
 // TODO: Color Guide, 폰트 설정 세팅되면 색상, 폰트 설정 변경
+// MARK: Apple Login
 extension LoginView {
     var appleLoginButton: some View {
         ZStack {
@@ -33,16 +49,38 @@ extension LoginView {
         .cornerRadius(16)
         .overlay {
             SignInWithAppleButton(
-                onRequest: {
-                    request in
-                },
-                onCompletion: { result in
-                })
+                onRequest: appleLoginOnRequest,
+                onCompletion: appleLoginOnCompletion
+            )
             .blendMode(.overlay)
+        }
+    }
+    
+    private func appleLoginOnRequest(request: ASAuthorizationAppleIDRequest) {
+        request.requestedScopes = [.email, .fullName]
+    }
+    
+    private func appleLoginOnCompletion(result: Result<ASAuthorization, Error>) {
+        switch result {
+        case .success(let authResults):
+            switch authResults.credential {
+            case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                let fullName = appleIDCredential.fullName
+                let name =  (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
+                let email = appleIDCredential.email
+                let identityToken = String(data: appleIDCredential.identityToken!, encoding: .utf8)!
+                let newUser = UserFromLoginProvider(loginProvider: .apple, providerId: identityToken, name: name, email: email)
+                userTrigger.send(newUser)
+            default:
+                break
+            }
+        case .failure(let error):
+            print(error.localizedDescription)
         }
     }
 }
 
 #Preview {
-    LoginView()
+    let vm: LoginViewModel = LoginAssemblerImpl().resolve()
+    return LoginView(vm: vm)
 }
