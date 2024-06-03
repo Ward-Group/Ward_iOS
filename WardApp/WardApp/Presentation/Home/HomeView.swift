3889ab5317309f746fa7c87889ca4ecc879a22e6
SuniDev <sun-918@naver.com>
2024-06-03 11:23:39 +0900

    refactor : 홈 화면 Router 생성, 네비게이션 영역 추가
    #48

--- WardApp/WardApp/Presentation/Home/HomeView.swift
+++ WardApp/WardApp/Presentation/Home/HomeView.swift
@@ -11,11 +11,13 @@ import Combine
 struct HomeView: View {
     @ObservedObject var output: HomeViewModel.Output
     
+    @State var router: HomeRouter
     private let viewModel: HomeViewModel
     private let cancelBag = CancelBag()
     private let loadTrigger = PassthroughSubject<Void, Never>()
         
-    init(viewModel: HomeViewModel) {
+    init(router: HomeRouter, viewModel: HomeViewModel) {
+        self.router = router
         self.viewModel = viewModel
         let input = HomeViewModel.Input(loadTrigger: loadTrigger.asDriver())
         self.output = viewModel.transform(input, cancelBag: cancelBag)
@@ -24,17 +26,17 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            HeaderNavBarView(showSearching: true, showNotification: true)
+            naviBarView
             GeometryReader { geo in
                 ScrollView(.vertical, showsIndicators: true, content: {
-                    LazyVStack(spacing: 0) {
+                    VStack(spacing: 0) {
                         // --- 오늘 마감 --- //
-                        HomeHeaderView(title: WardStrings.dueToday, subTitle: WardStrings.enjoyTheLittleLuckYouHaveLeft)
+                        HomeHeaderTitleView(title: WardStrings.dueToday, subTitle: WardStrings.enjoyTheLittleLuckYouHaveLeft)
                             .padding([.bottom], 12)
                         BannerPageView(geo: geo, list: output.bannerPageList)
                             .padding([.bottom], 36)
                         // -- 발매 상품 -- //
-                        HomeHeaderView(title: WardStrings.releasedProduct, moreButtonAction: {
+                        HomeHeaderTitleView(title: WardStrings.releasedProduct, moreButtonAction: {
                             Log.todo("발매 상품 더보기 버튼 액션")
                         })
                         .padding([.bottom], 14)
@@ -47,6 +49,36 @@ struct HomeView: View {
     }
 }
 
+// MARK: - Sub View
+extension HomeView {
+    private var naviBarView: some View {
+        return HStack {
+            WardAssets.Image.wardLogo.swiftUIImage
+                .resizable()
+                .scaledToFit()
+                .frame(height: 24)
+            Spacer()
+            Button(action: {}, label: {
+                WardAssets.Image.Icon.searching.swiftUIImage
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            })
+            Spacer()
+                .frame(width: 8)
+            Button(action: {
+                router.present(fullScreen: .notification)
+            }, label: {
+                WardAssets.Image.Icon.notification.swiftUIImage
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            })
+        }
+        .padding(.horizontal, 16)
+        .background(WardAssets.Color.backgroundColor.swiftUIColor)
+        .frame(height: 54)
+    }
+}
+
 #Preview {
-    HomeView(viewModel: HomeViewModel())
+    HomeView(router: HomeRouter(), viewModel: HomeViewModel())
 }
