836ae8e51d4d13c0fa32639ce60adef5a9365050
peppermint100 <krpeppermint100@gmail.com>
2024-06-04 15:23:33 +0900

    feat: 공지사항 View, ViewModel

---
+++ WardApp/WardApp/Presentation/NoticeBoard/NoticeBoardView.swift
@@ -0,0 +1,83 @@
+//
+//  NoticeBoardView.swift
+//  WardApp
+//
+//  Created by peppermint100 on 6/4/24.
+//
+
+import SwiftUI
+import Combine
+
+struct NoticeBoardView: View {
+    
+    @ObservedObject private var input: NoticeBoardViewModel.Input
+    @ObservedObject private var output: NoticeBoardViewModel.Output
+    
+    private let openNoticeButtonTrigger = PassthroughSubject<UUID, Never>()
+    
+    private let cancelBag = CancelBag()
+    
+    init(vm: NoticeBoardViewModel) {
+        let input = NoticeBoardViewModel.Input(
+            openNoticeButtonTrigger: openNoticeButtonTrigger.asDriver()
+        )
+        let output = vm.transform(input, cancelBag: cancelBag)
+        self.input = input
+        self.output = output
+    }
+    
+    var body: some View {
+        ZStack {
+            Color.background
+            VStack {
+                InlineNavBarView(title: WardStrings.noticeBoard)
+                ScrollView {
+                    LazyVStack(spacing: 15) {
+                        noticeBoardList
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NoticeBoardView(vm: NoticeBoardAssemblerImpl().resolve())
+}
+
+extension NoticeBoardView {
+    
+    private var noticeBoardList: some View {
+        ForEach($output.items, id: \.id) { item in
+            DisclosureGroup(isExpanded: item.isCollapsed) {
+                Text(item.wrappedValue.content)
+                    .font(WardFonts.Pretendard.regular.swiftUIFont(size: 14))
+                    .foregroundStyle(Color.black0)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+            } label: {
+                HStack {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(item.wrappedValue.title)
+                            .font(WardFonts.Pretendard.bold.swiftUIFont(size: 16))
+                            .foregroundStyle(Color.black0)
+                            .lineLimit(1)
+                        
+                        Text(item.wrappedValue.createdAt.format(formatString: "yyyy-MM-dd"))
+                            .font(WardFonts.Pretendard.semiBold.swiftUIFont(size: 14))
+                            .foregroundStyle(Color.white3)
+                    }
+                    
+                    Spacer()
+                    
+                    WardAssets.Image.Icon.chevronDown.swiftUIImage
+                        .rotationEffect(Angle(degrees: item.wrappedValue.isCollapsed ? 180 : 0))
+                }
+                /// DisclosureGroup의 기본 버튼을 안보이게 하게 위해 accentColor를 .clear로 주고 뒤에 버튼에 있기 때문에 leading padding만 적용했습니다.
+                .padding(.leading)
+            }
+            .accentColor(.clear)
+            Divider()
+        }
+    }
+}
