6ed7578b22fcb7b3de204c93dd972b6639a12378
peppermint100 <krpeppermint100@gmail.com>
2024-06-07 19:23:14 +0900

    feat: 토큰 저장용 UserDefaults

--- WardApp/WardApp/Support/UserDefaults/UserDefaultsKey.swift
+++ WardApp/WardApp/Support/UserDefaults/UserDefaultsKey.swift
@@ -9,4 +9,9 @@ import Foundation
 
 enum UserDefaultsKey {
     static let searchHistory = "searchHistory"
+    
+    static let loginProvider = "loginProvider"
+    static let accessToken = "accessToken"
+    static let refreshToken = "refreshToken"
+    static let email = "email"
 }
