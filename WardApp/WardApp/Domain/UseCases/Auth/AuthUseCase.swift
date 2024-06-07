4adf2c9cdc12bdec3d9fb20383d1309231361655
peppermint100 <krpeppermint100@gmail.com>
2024-06-07 22:31:10 +0900

    feat: 로그인 메소드 추가로 작성(로그인 뷰용)

--- WardApp/WardApp/Domain/UseCases/Auth/AuthUseCase.swift
+++ WardApp/WardApp/Domain/UseCases/Auth/AuthUseCase.swift
@@ -17,9 +17,16 @@ struct AuthUseCase {
         return repository.login(dto: dto)
     }
     
+    func login(provider: LoginProvider, providerId: String, email: String) -> AnyPublisher<LoginResponse, AuthError> {
+        let dto = LoginDto(provider: provider, providerId: providerId, email: email)
+        Log.debug("\(dto.provider) - \(dto.providerId) - \(String(describing: dto.email)) 으로 로그인합니다.")
+        updateLoginDto(dto: dto)
+        return repository.login(dto: dto)
+    }
+    
     private func loadLoginDto() -> LoginDto {
         let provider = UserRepository.shared.getLoginProvider()
-        let providerId = UserRepository.shared.getAccessToken()
+        let providerId = UserRepository.shared.getProviderId()
         let email = UserRepository.shared.getEmail()
         
         return LoginDto(provider: provider, providerId: providerId, email: email)
@@ -28,5 +35,11 @@ struct AuthUseCase {
     func updateToken(accessToken: String, refreshToken: String) {
         UserRepository.shared.updateAcceesToken(accessToken)
         UserRepository.shared.updateRefreshToken(refreshToken)
+    }
+    
+    func updateLoginDto(dto: LoginDto) {
+        UserRepository.shared.updateEmail(dto.email)
+        UserRepository.shared.updateLoginProvider(dto.provider)
+        UserRepository.shared.updateLoginProvicerId(dto.providerId)
     }
 }
