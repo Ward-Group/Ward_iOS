0602b825e62f4df3639b65185d129ee38b459d22
peppermint100 <krpeppermint100@gmail.com>
2024-06-24 16:00:32 +0900

    fix: MyPageView 공통 컴포넌트 대체

--- WardApp/WardApp/Presentation/MyPage/MyPageView.swift
+++ WardApp/WardApp/Presentation/MyPage/MyPageView.swift
@@ -170,34 +170,28 @@ private extension MyPageView {
     
     var myActivity: some View {
         VStack(alignment: .leading) {
-            Text(WardStrings.myActivity)
-                .font(WardFonts.Pretendard.bold.swiftUIFont(size: 14))
-                .foregroundStyle(Color.black1)
-                .padding(.bottom, 15)
+            SectionHeaderView(title: WardStrings.myActivity)
             
-            MyPageNavigatorView(title: WardStrings.interestedList)
+            SectionListItemView(title: WardStrings.interestedList)
                 .onTapGesture {
                     mainRouter.changeTab(to: .interested)
                 }
-            MyPageNavigatorView(title: WardStrings.submitReview)
+            SectionListItemView(title: WardStrings.submitReview)
         }
     }
     
     var etc: some View {
         VStack(alignment: .leading) {
-            Text("기타")
-                .font(WardFonts.Pretendard.bold.swiftUIFont(size: 14))
-                .foregroundStyle(Color.black1)
-                .padding(.bottom, 15)
+            SectionHeaderView(title: WardStrings.etc)
             
-            MyPageNavigatorView(title: WardStrings.noticeBoard)
+            SectionListItemView(title: WardStrings.noticeBoard)
                 .onTapGesture {
                     router.push(.noticeBoard)
                 }
             
-            MyPageNavigatorView(title: WardStrings.oneOnOneInquries)
+            SectionListItemView(title: WardStrings.oneOnOneInquries)
             
-            MyPageNavigatorView(title: WardStrings.logOut)
+            SectionListItemView(title: WardStrings.logOut)
                 .onTapGesture {
                     logOutButtonTrigger.send()
                 }
